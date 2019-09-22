78ce0a2c71375a873a1f78d0a8d5155efe85cda5
Glubs9 <37764604+Glubs9@users.noreply.github.com>
2019-09-22 12:34:21 +1000

    Update PixelCanvas.cs
    made the pixelcanvas not static

--- PixelCanvas.cs
+++ PixelCanvas.cs
@@ -2,14 +2,14 @@ using System;
 using System.Linq;
 
 namespace PixelCanvasInclude{
-static class PixelCanvas{
+class PixelCanvas{
 
-    private static ConsoleColor[,] board;
-    private static int boardHeight; 
-    private static int boardWidth;
-    private static ConsoleColor background;
+    private ConsoleColor[,] board; 
+    private int boardHeight; 
+    private int boardWidth;
+    private ConsoleColor background;
 
-    public static void Init(int boardWidthIn, int boardHeightIn, ConsoleColor BaseColour){
+    public PixelCanvas(int boardWidthIn, int boardHeightIn, ConsoleColor BaseColour){
 
         boardHeight = boardHeightIn;
         boardWidth = boardWidthIn;
@@ -19,7 +19,7 @@ static class PixelCanvas{
 
     }
 
-    public static void Output(){
+    public void Output(){
 
         for (int y = 0; y < boardHeight; y++){
             for (int x = 0; x < boardWidth; x++){
@@ -32,24 +32,24 @@ static class PixelCanvas{
 
     }
 
-    public static void OutPutAndClear(){
+    public void OutPutAndClear(){
         Console.Clear();
         Output();
     }
 
-    public static void SetPixel(int x, int y, ConsoleColor color){
+    public void SetPixel(int x, int y, ConsoleColor color){
         board[x,y] = color;
     }
 
-    public static ConsoleColor GetPixel(int x, int y){
+    public ConsoleColor GetPixel(int x, int y){
         return board[x,y];
     }
 
-    public static void DeletePixel(int x, int y){
+    public void DeletePixel(int x, int y){
         SetPixel(x,y,background);
     }
 
-    public static void DrawRect(int xin, int yin, int width, int height, ConsoleColor color){
+    public void DrawRect(int xin, int yin, int width, int height, ConsoleColor color){
         for (int y = yin; y < yin + height; y++){
             for (int x = xin; x < xin + width; x++){
                 board[x,y] = color;
@@ -57,28 +57,17 @@ static class PixelCanvas{
         }
     }
 
-    public static void ClearRect(int xin, int yin, int width, int height){
+    public void ClearRect(int xin, int yin, int width, int height){
         DrawRect(xin,yin,width,height,background);
     }
 
-    public static void Clear(){
+    public void Clear(){
         ClearRect(0,0,boardWidth,boardHeight);
     }
 
-    public static void SetBackground(ConsoleColor BackgroundInput){
+    public void SetBackground(ConsoleColor BackgroundInput){
         background = BackgroundInput;
     }
-
-    /*public static void Main(string[] args){
-        Init(50,50,ConsoleColor.Black);
-        Output();
-        SetPixel(5,5,ConsoleColor.Cyan);
-        Output();
-        DrawRect(25,25,10,10,ConsoleColor.White);
-        Output();
-        Clear();
-        Output();
-    }*/
 
 }
 }
